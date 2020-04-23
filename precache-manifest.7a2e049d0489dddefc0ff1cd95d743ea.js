140932f5d4ea12e57d1bdf1edd02e12498e1daaf
Soheil Tehrani <s.tehrani1994@gmail.com>
2020-04-23 12:19:24 +0430

    Updates

---
+++ precache-manifest.7a2e049d0489dddefc0ff1cd95d743ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1467d085d432abc6b746562d01df859",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb907b474ac4f155b9e1",
+    "url": "/monsters-rolodex/static/css/main.3dee2b44.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb907b474ac4f155b9e1",
+    "url": "/monsters-rolodex/static/js/main.8f02e101.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
